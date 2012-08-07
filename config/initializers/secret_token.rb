b3361e11da42c57a587c7b4c5de90d53c4547979
vijay <documentlistener@gmail.com>
2012-08-08 00:57:33 +0530

    Adding to Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webservice::Application.config.secret_token = '89b15f713c6c522c738d635f1c8598cc58feaa034a903ebe5e303ecc413b0ee164552d0231c6f66e5477a1da138a05083bde7abee828af9c0b5146d8cffde4c9'
